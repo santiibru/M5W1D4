312e3fce06f4b3ac0aa83649d79da3cbe7e8d534
Santiago Bruno <santiago.p.bruno@gmail.com>
2024-03-11 18:56:26 +0100

    2commit

--- m5w1/src/AllTheBooks.jsx
+++ m5w1/src/AllTheBooks.jsx
@@ -2,15 +2,16 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import Books from './data/horror.json';
 import Col from 'react-bootstrap/esm/Col';
+import './App.css';
 
 function AllTheBooks() {
     return (
         Books.map((el) => (
-            <Col>
-                <Card className="m-2" style={{ width: '15rem'}}>
-                    <Card.Img variant="top" src={el.img} />
+            <Col lg={3} md={4} sm={6}>
+                <Card className="my-2">
+                    <Card.Img className="card-img" variant="top" src={el.img} />
                     <Card.Body>
-                        <Card.Title className="fs-6">{el.title}</Card.Title>
+                        <Card.Title className="fs-6 card-title">{el.title}</Card.Title>
                         <Card.Text>{"$" + el.price}</Card.Text>
                     </Card.Body>
                 </Card>
